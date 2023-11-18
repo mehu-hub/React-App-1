6c3d72c950bf0c8e998746827f5f996fc367086a
Mehedi Hasan <designwithmehedi@gmail.com>
2023-11-19 02:02:28 +0600

    Create Own Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
+      <Person2></Person2>
+      <Person></Person>
+      <Person2></Person2>
+      <Person></Person>
+      <Person2></Person2>
+      <Person></Person>
+      <Person2></Person2>
     </div>
   );
+}
+
+function Person() {
+  return (
+    <div className='person'>
+      <h2>Mehedi Hasan</h2>
+    </div>
+  )
+}
+
+function Person2() {
+  return (
+    <div className='person2'>
+      <h2>Hasan Mehedi</h2>
+    </div>
+  )
 }
 
 export default App;
