450ee1edd31db60c9520d6f9dbb0a32bb51cafe8
Mehedi Hasan <designwithmehedi@gmail.com>
2023-11-19 02:23:05 +0600

    Pass dynamic data to components

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <Person name="Mehedi Hasan" age="21"></Person>
+      <Person name="Hasan" age="20"></Person>
+      <Person name="Omar Faruk" age="22"></Person> 
+      <Person name="Ali Vai" age="25"></Person>
       <Person></Person>
-      <Person2></Person2>
-      <Person></Person>
-      <Person2></Person2>
-      <Person></Person>
-      <Person2></Person2>
-      <Person></Person>
-      <Person2></Person2>
+       
     </div>
   );
 }
 
-function Person() {
+function Person(props) {
+  console.log(props)
   return (
     <div className='person'>
-      <h2>Mehedi Hasan</h2>
+      <h2>Name: {props.name}</h2>
+      <p>Age: {props.age}</p>
     </div>
   )
 }
