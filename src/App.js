028db159062b03a720b34c69cde8c06c0b2bcc84
Mehedi Hasan <designwithmehedi@gmail.com>
2023-11-19 11:11:44 +0600

    Load User Data From API Using useEffect Hooks in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() { 
-  const [count, setCount] = useState(0)
+function App() {
 
-  const increaseCount = () => setCount(count + 1);
-  const decreaseCount = () => setCount(count - 1);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setUsers(data))
+
+  }, [])
 
   return (
     <div className="App">
-      <div className='count'>
-        <h2>Count: {count}</h2>
-        <button onClick={increaseCount}>Increase</button>
-        <button onClick={decreaseCount}>Decrease</button>
-      </div>
-
+      <h2>Load User From API</h2>
+      {
+        users.map(user => <Users name={user.name} email={user.email}></Users>)
+      }
     </div>
   );
 }
 
+function Users(props) {
+  return (
+    <div className='user-con'>
+      <div className='user'>
+        <h4>User: {props.name}</h4>
+        <p>Email: {props.email}</p>
+      </div>
+    </div>
+  )
+}
+
+// const [count, setCount] = useState(0)
+// const increaseCount = () => setCount(count + 1);
+// const decreaseCount = () => setCount(count - 1);
+{/* <h2>Count: {count}</h2>
+<button onClick={increaseCount}>Increase</button>
+<button onClick={decreaseCount}>Decrease</button> */}
 
 export default App;
